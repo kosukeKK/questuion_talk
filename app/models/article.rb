1f69262da25a60317e7ef1bc9d3cc9ae8712cf94
kosukeKK <koskacts@gmail.com>
2018-07-29 19:46:50 +0900

    fix

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,10 @@
 class Article < ApplicationRecord
+  # Relation ship
   belongs_to :user
   has_many :article_comments
+
+  # validation
+  validates :title, presence: true, length: { in: 8..140 }
+  validates :description, length: { maximum: 255 }
+  validates :flg_under_18, presence: true, inclusion: { in: [true, false] }
 end
--- app/models/article_comment.rb
+++ app/models/article_comment.rb
@@ -1,3 +1,7 @@
 class ArticleComment < ApplicationRecord
+  # Relation ship
   belongs_to :article
+
+  # Validation
+  validates :body, length: { maximum: 140 }
 end
