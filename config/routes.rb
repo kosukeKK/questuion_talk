1ccc384266f5f367507016111fb4588689864f94
kosukeKK <koskacts@gmail.com>
2018-07-29 19:46:50 +0900

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "top#index"
-  resources :articles, :article_comments
+  resources :articles
 
-  post '/article_comments/create/:id', to: 'articles_comments#create_comment', as: 'create_comment'
+  get '/article_comments/:id', to: 'articles_comments#show', as: 'article_comments'
+  post '/article_comments/:id', to: 'articles_comments#create', as: 'create_article_comments'
+  delete '/article_comments/:id', to: 'articles_comments#destroy', as: 'destroy_article_comments'
 end
